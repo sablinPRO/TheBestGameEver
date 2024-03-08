bcdbe3cdc2d59216b6bed42d942e7f29082bb345
sablinPRO <mikegrizly@yandex.ru>
2024-03-08 13:30:49 +0300

    gg

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+    private void Start()
+    {
+       InitComponentLinks();
+       PickNewPatrolPoints();
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+
+    private void Update()
+    {
+        NoticePlayerUpdate();
+        PatrolUpdate();
+    }
+    private void NoticePlayerUpdate()
+
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }              
+            }           
+        }      
+    }
+   
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoints();
+        }
+    }
+
+    private void PickNewPatrolPoints()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+}
